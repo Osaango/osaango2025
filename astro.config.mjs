19e48378b5103513cc7eb0d27a6fd8b39b0ad055
Marjukka Niinioja <mniinio@users.noreply.github.com>
2025-11-30 10:58:07 +0200

    adding Netlify CDN and further image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+import netlify from '@astrojs/netlify';
+
+export default defineConfig({
+  adapter: netlify(),
+  image: {
+                service: {
+                        entrypoint: '@astrojs/netlify/image-service',
+                },
+        },
+});
