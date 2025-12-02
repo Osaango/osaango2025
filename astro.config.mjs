aa86b23fac23ff87e27e90a53655d238ddfc4644
Marjukka Niinioja <mniinio@users.noreply.github.com>
2025-12-02 18:19:48 +0200

    Fix structured data canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
+  site: 'https://www.osaango.com',
   adapter: netlify(),
   image: {
                 service: {
